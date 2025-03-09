d0a62257441f938fbe6bc0c990162d4a5d5df85e
tmddnxx <lsw6878@naver.com>
2025-03-09 21:15:43 +0900

    123

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
+
 };
 
 export default nextConfig;
