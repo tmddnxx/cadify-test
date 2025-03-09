fb41e2a81f96416aa00759137c9915bf97c43b6b
tmddnxx <lsw6878@naver.com>
2025-03-09 20:30:32 +0900

    aa

---
+++ src/app/project/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function ProjectPage() {
+    return (
+      <div style={{width:'100%', height: '100vh'}}>
+        <div style={{width: 'auto', height: 'auto', margin: 'auto 0px'}}>
+            <h1 style={{width:'auto', height: 'auto', display: 'inline'}}>프로젝트 페이지</h1>
+            <p style={{width:'auto', height: 'auto', display: 'inline'}}>이곳에서 프로젝트를 업로드하세요.</p>
+        </div>
+      </div>
+    );
+  }
+  
